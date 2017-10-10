666c9c1a92ff7e832df580cba1c01948739985d3
Iorrah Gonzalez <iorrah.gonzalez@gmail.com>
2017-10-10 12:41:49 +0200

    Homepage: implementing a simple Search Feature and revising gallery states

--- app/components/pt-main.js
+++ app/components/pt-main.js
@@ -2,12 +2,30 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'main',
+  filters: {
+    name: null,
+    liked: null,
+    desliked: null
+  },
+  filteredTrees: Ember.computed('filters.name', 'trees.@each', function() {
+    let name = (this.get('filters.name') || '').toLowerCase();
+    let trees = this.get('trees');
+
+    if (name) {
+      trees = trees.filter(function(e) {
+        return (e.name.toLowerCase().indexOf(name) > -1) ||
+               (e.species_name.toLowerCase().indexOf(name) > -1);
+      });
+    }
+
+    return trees;
+  }),
   trees: [
     {
       "name": "Baobab",
       "species_name": "Adansonia",
       "image": "https://upload.wikimedia.org/wikipedia/commons/3/36/Baobab_Adansonia_digitata.jpg",
-      "is_img_shown": true,
+      "is_img_shown": false,
       "likes": 0,
       "deslikes": 0
     },
@@ -15,7 +33,7 @@ export default Ember.Component.extend({
       "name": "Red Mangrove",
       "species_name": "Rhizophora mangle",
       "image": "https://upload.wikimedia.org/wikipedia/en/1/16/Red_mangrove-everglades_natl_park.jpg",
-      "is_img_shown": true,
+      "is_img_shown": false,
       "likes": 0,
       "deslikes": 0
     },
@@ -23,7 +41,7 @@ export default Ember.Component.extend({
       "name": "Common Hornbeam",
       "species_name": "Carpinus betulus",
       "image": "https://upload.wikimedia.org/wikipedia/commons/2/2c/Carpinus_betulus_-_Hunsr%C3%BCck_001.jpg",
-      "is_img_shown": true,
+      "is_img_shown": false,
       "likes": 0,
       "deslikes": 0
     },
@@ -31,7 +49,7 @@ export default Ember.Component.extend({
       "name": "Turkey Oak",
       "species_name": "Quercus cerris",
       "image": "https://upload.wikimedia.org/wikipedia/commons/3/34/Quercus_cerris.JPG",
-      "is_img_shown": true,
+      "is_img_shown": false,
       "likes": 0,
       "deslikes": 0
     },
@@ -39,7 +57,7 @@ export default Ember.Component.extend({
       "name": "Japanese red pine",
       "species_name": "Pinus densiflora",
       "image": "https://upload.wikimedia.org/wikipedia/commons/f/f0/Pinus_syluestriformis_%28Takenouchi%29T.Wang_ex_Cheng.JPG",
-      "is_img_shown": true,
+      "is_img_shown": false,
       "likes": 0,
       "deslikes": 0
     },
@@ -47,7 +65,7 @@ export default Ember.Component.extend({
       "name": "Baobab",
       "species_name": "Adansonia",
       "image": "https://upload.wikimedia.org/wikipedia/commons/3/36/Baobab_Adansonia_digitata.jpg",
-      "is_img_shown": true,
+      "is_img_shown": false,
       "likes": 0,
       "deslikes": 0
     },
@@ -55,7 +73,7 @@ export default Ember.Component.extend({
       "name": "Red Mangrove",
       "species_name": "Rhizophora mangle",
       "image": "https://upload.wikimedia.org/wikipedia/en/1/16/Red_mangrove-everglades_natl_park.jpg",
-      "is_img_shown": true,
+      "is_img_shown": false,
       "likes": 0,
       "deslikes": 0
     },
@@ -63,7 +81,7 @@ export default Ember.Component.extend({
       "name": "Common Hornbeam",
       "species_name": "Carpinus betulus",
       "image": "https://upload.wikimedia.org/wikipedia/commons/2/2c/Carpinus_betulus_-_Hunsr%C3%BCck_001.jpg",
-      "is_img_shown": true,
+      "is_img_shown": false,
       "likes": 0,
       "deslikes": 0
     },
@@ -71,7 +89,7 @@ export default Ember.Component.extend({
       "name": "Turkey Oak",
       "species_name": "Quercus cerris",
       "image": "https://upload.wikimedia.org/wikipedia/commons/3/34/Quercus_cerris.JPG",
-      "is_img_shown": true,
+      "is_img_shown": false,
       "likes": 0,
       "deslikes": 0
     },
@@ -79,7 +97,7 @@ export default Ember.Component.extend({
       "name": "Japanese red pine",
       "species_name": "Pinus densiflora",
       "image": "https://upload.wikimedia.org/wikipedia/commons/f/f0/Pinus_syluestriformis_%28Takenouchi%29T.Wang_ex_Cheng.JPG",
-      "is_img_shown": true,
+      "is_img_shown": false,
       "likes": 0,
       "deslikes": 0
     },
@@ -87,7 +105,7 @@ export default Ember.Component.extend({
       "name": "Baobab",
       "species_name": "Adansonia",
       "image": "https://upload.wikimedia.org/wikipedia/commons/3/36/Baobab_Adansonia_digitata.jpg",
-      "is_img_shown": true,
+      "is_img_shown": false,
       "likes": 0,
       "deslikes": 0
     },
@@ -95,7 +113,7 @@ export default Ember.Component.extend({
       "name": "Red Mangrove",
       "species_name": "Rhizophora mangle",
       "image": "https://upload.wikimedia.org/wikipedia/en/1/16/Red_mangrove-everglades_natl_park.jpg",
-      "is_img_shown": true,
+      "is_img_shown": false,
       "likes": 0,
       "deslikes": 0
     },
@@ -103,7 +121,7 @@ export default Ember.Component.extend({
       "name": "Common Hornbeam",
       "species_name": "Carpinus betulus",
       "image": "https://upload.wikimedia.org/wikipedia/commons/2/2c/Carpinus_betulus_-_Hunsr%C3%BCck_001.jpg",
-      "is_img_shown": true,
+      "is_img_shown": false,
       "likes": 0,
       "deslikes": 0
     },
@@ -111,7 +129,7 @@ export default Ember.Component.extend({
       "name": "Turkey Oak",
       "species_name": "Quercus cerris",
       "image": "https://upload.wikimedia.org/wikipedia/commons/3/34/Quercus_cerris.JPG",
-      "is_img_shown": true,
+      "is_img_shown": false,
       "likes": 0,
       "deslikes": 0
     },
@@ -119,7 +137,7 @@ export default Ember.Component.extend({
       "name": "Japanese red pine",
       "species_name": "Pinus densiflora",
       "image": "https://upload.wikimedia.org/wikipedia/commons/f/f0/Pinus_syluestriformis_%28Takenouchi%29T.Wang_ex_Cheng.JPG",
-      "is_img_shown": true,
+      "is_img_shown": false,
       "likes": 0,
       "deslikes": 0
     }
