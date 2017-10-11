4b585aa8c3210304bbb8cb402f249013838819e7
Iorrah Gonzalez <iorrah.gonzalez@gmail.com>
2017-10-11 22:16:49 +0200

    Acceptance Tests: implementing a Trees Search test

---
+++ tests/acceptance/search-trees.js
@@ -0,0 +1,13 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'you-rockstar/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | Search Trees');
+
+test('search-trees', function(assert) {
+  visit('/');
+  const TREE_NAME = 'red';
+  let treeParagSelector = '.gallery-items a .caption.bottom p:contains(' + TREE_NAME + ')';
+  let amountTrees = $('.gallery-items').find(treeParagSelector).length;
+  fillIn('header .input-search', TREE_NAME);
+  andThen(() => assert.equal(amountTrees, $('.gallery-items a').length));
+});
