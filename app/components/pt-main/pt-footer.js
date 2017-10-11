c2da87fa40464c10dc0547f6c88d0e05a941167c
Iorrah Gonzalez <iorrah.gonzalez@gmail.com>
2017-10-11 18:58:54 +0200

    Footer: ensuring that we start the tooltips and replacing SN item

--- app/components/pt-main/pt-footer.js
+++ app/components/pt-main/pt-footer.js
@@ -2,4 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'footer',
+  didInsertElement() {
+    this.startTooltips();
+  },
+  startTooltips() {
+    $('[data-toggle="tooltip"]').tooltip();
+  }
 });
